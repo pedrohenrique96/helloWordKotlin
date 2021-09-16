0f29b347df1a7b3fa6a8bc6f0ebf2be43db68065
Pedro Henrique <pedrook16@gmail.com>
2021-09-15 21:23:57 -0300

    hello completed

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,18 @@
-fun main() {
-    println("What's your first name?")
+fun hello() {
+    println("What's your name?")
     val name = readLine()
 
-    println("What's your lastName?")
-    val lastName = readLine()
-    println("Hello $name $lastName!")
+    println("What's your email?")
+    val email = readLine()
+
+    println("Hello $name, I am Robot, thanks for connected, i going send e-mail for this $email, ok ?")
+
+    println("You can confirm? Ok or No")
+    val confirmed = readLine()
+
+    println("Thanks for the connection $confirmed!")
+}
+
+fun main() {
+    hello()
 }
